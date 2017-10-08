5672fa40be12ced68db4720c51830689c02adabd
Eric Laursen <ericjlaursen@gmail.com>
2017-10-08 10:46:30 -0400

    includes test for onSubmit!

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import GuessForm from './guess-form.js';
 
-describe('<GuessForm />', () => {
-	it('Renders without crashing'), () => {
+import GuessForm from './guess-form';
+
+describe('<GuessForm/>', () => {
+	it('Renders without crashing', () => {
 		shallow(<GuessForm />);
-	}
+	})
+
+	it('fires the onGuess function when submitted', () => {
+		let testOnGuess = jest.fn();
+		let component = mount(<GuessForm onGuess={testOnGuess}/>);
+		let testGuessVal = 84;
+		component.find('input[type="text"]').node.value = testGuessVal;
+		component.simulate('submit');
+		expect(testOnGuess).toHaveBeenCalledWith(testGuessVal.toString());
+	})
 })
