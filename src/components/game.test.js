3b424ce9151ecae06d4cb69bf1911bb6ba5c9db1
Eric Laursen <ericjlaursen@gmail.com>
2017-10-08 08:31:22 -0400

    includes newGame test and guess test

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -4,10 +4,11 @@ import {shallow} from 'enzyme';
 import Game from './game';
 
 describe('<Game/>', () => {
-    let seedLists = [];
+    let seedGuesses = [];
     beforeAll(() => {
         for (let i=0; i<10; i++) {
-            seedLists.push(`List ${i}`)
+            seedGuesses.push(`feedback`);
+            seedGuesses.push(`${i}`);
         }
     });
 
@@ -15,22 +16,39 @@ describe('<Game/>', () => {
         shallow(<Game title="Foo" />);
     });
 
-    // it('Can add lists to the state', () => {
-    //     const wrapper = shallow(<Game/>);
-    //     const instance = wrapper.instance();
-    //     seedLists.forEach(instance.addList);
-    //     expect(wrapper.state('lists').length).toEqual(seedLists.length);
-    // });
+    it('Can start a new game', () => {
+        //1. get/declare the component
+        const wrapper =shallow(<Game />);
 
-    // it('Renders the lists', () => {
-    //     const wrapper = shallow(<Game/>);
-    //     const instance = wrapper.instance();
-    //     seedLists.forEach(instance.addList);
-    //     const lists = wrapper.find('List');
-    //     expect(lists.length).toEqual(seedLists.length);
-    //     const firstList = lists.first();
-    //     expect(firstList.prop('title')).toEqual(seedLists[0]);
-    // });
+        //2. set the default state to something non-sensical
+        wrapper.setState({
+            correctAnswer : -5//-5, nonsensical
+        });
+
+        //3. run the newGame  function
+        wrapper.instance().newGame();
+
+        //4. Check to see if the newGame has a different correctAnswer than previously set
+        expect(wrapper.state('correctAnswer')).toBeGreaterThan(0);
+    })
+
+     it('Can make a guess', () => {
+        const wrapper =shallow(<Game />);
+
+        //set a correct answer
+        wrapper.setState({
+            correctAnswer : 84
+        });
+
+        //make a guess
+        wrapper.instance().guess(10);
+
+        //check the state for a new registered guess
+        expect(wrapper.state('guesses')).toEqual([10]);
+
+        //checks that feedback is COLD!
+        expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...');
+    });
 });
 
 
