2d48ca9cec411db9040fa41124756dc5f1a6e944
Eric Laursen <ericjlaursen@gmail.com>
2017-10-08 10:16:45 -0400

    updates it title

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -5,7 +5,7 @@ import GuessList from './guess-list';
 
 describe('<GuessList/>', () => {
 	let testGuesses = [1,2,3,4,5];
-	it('Renders without crashing', () =>{
+	it('Renders with sample guesses in prop without crashing', () =>{
 		shallow(<GuessList guesses={testGuesses}/>)
 	})
 })
